f1fede97504583d79de469fbd1ca01327724aeab
Angel <angelg2181@icloud.com>
2023-09-12 10:49:43 -0400

    fixed typo

--- packet/chat_command.go
+++ packet/chat_command.go
@@ -1,6 +1,6 @@
 package packet
 
-type ChatComamndServer struct {
+type ChatCommandServer struct {
 	Command   string
 	Timestamp int64
 	Salt      int64
@@ -9,17 +9,17 @@ type ChatComamndServer struct {
 	//https://wiki.vg/Protocol#Chat_Command
 }
 
-func (m ChatComamndServer) ID() int32 {
+func (m ChatCommandServer) ID() int32 {
 	return 0x04
 }
 
-func (m *ChatComamndServer) Decode(r *Reader) error {
+func (m *ChatCommandServer) Decode(r *Reader) error {
 	r.String(&m.Command)
 	r.Int64(&m.Timestamp)
 	return r.Int64(&m.Salt)
 }
 
-func (m ChatComamndServer) Encode(w Writer) error {
+func (m ChatCommandServer) Encode(w Writer) error {
 	w.String(m.Command)
 	w.Int64(m.Timestamp)
 	return w.Int64(m.Salt)
