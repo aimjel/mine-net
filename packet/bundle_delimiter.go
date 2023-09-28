f477127f6069befddfd89dbbbff09077c0e002df
Angel <angelg2181@icloud.com>
2023-09-28 13:59:11 -0400

    Create bundle_delimiter.go

---
+++ packet/bundle_delimiter.go
@@ -0,0 +1,15 @@
+package packet
+
+type BundleDelimiter struct{}
+
+func (b BundleDelimiter) ID() int32 {
+	return 0x00
+}
+
+func (b BundleDelimiter) Decode(r *Reader) error {
+	return nil
+}
+
+func (b BundleDelimiter) Encode(w Writer) error {
+	return nil
+}
