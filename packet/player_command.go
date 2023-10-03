2c173c557251d64b224fb63d93b704314c90ec1a
oq <84847714+oq-x@users.noreply.github.com>
2023-10-03 20:41:10 +0300

    Create player_command.go

---
+++ packet/player_command.go
@@ -0,0 +1,23 @@
+package packet
+
+type PlayerCommandServer struct {
+  EntityID int32
+  ActionID int32
+  JumpBoost int32
+}
+
+func (m PlayerCommandServer) ID() int32 {
+	return 0x1E
+}
+
+func (m *PlayerCommandServer) Decode(r *Reader) error {
+	r.VarInt(&m.EntityID)
+	r.VarInt(&m.ActionID)
+	return r.VarInt(&m.JumpBoost)
+}
+
+func (m PlayerCommandServer) Encode(w Writer) error {
+	w.VarInt(m.EntityID)
+	w.VarInt(m.ActionID)
+	return w.VarInt(m.JumpBoost)
+}
