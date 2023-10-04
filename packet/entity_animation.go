29ea10c26e9a09c20103d41d8d5b76325eb62600
oq <84847714+oq-x@users.noreply.github.com>
2023-10-04 20:15:06 +0300

    Entity Animation

---
+++ packet/entity_animation.go
@@ -0,0 +1,20 @@
+package packet
+
+type EntityAnimation struct {
+  EntityID int32
+  Animation uint8
+}
+
+func (a EntityAnimation) ID() int32 {
+	return 0x04
+}
+
+func (a *EntityAnimation) Decode(r *Reader) error {
+  r.VarInt(&a.EntityID)
+	return r.Uint8(&a.Animation)
+}
+
+func (a EntityAnimation) Encode(w Writer) error {
+  w.VarInt(a.EntityID)
+	return w.Uint8(a.Animation)
+}
