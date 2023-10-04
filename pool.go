ce0bc40d6a011cdc94baabdc7be033eefa149d96
oq <84847714+oq-x@users.noreply.github.com>
2023-10-04 20:33:31 +0300

    Update pool.go

--- pool.go
+++ pool.go
@@ -42,7 +42,7 @@ var serverBoundPlayPool = map[int32]func() packet.Packet{
 
 	0x30: func() packet.Packet { return &packet.TeleportToEntityServer{} },
 	
-	0x2F: func() packet.Packet { return &packet.SwingArmServer{} },
+	0x2F: func() packet.Packet { return &packet.SwingArm{} },
 
 	0x31: func() packet.Packet { return &packet.PlayerBlockPlacement{} },
 	0x32: func() packet.Packet { return &packet.UseItem{} },
