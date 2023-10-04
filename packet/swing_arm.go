6019f2e34c1aac6ca77136a9d9e31e6bb0da8802
oq <84847714+oq-x@users.noreply.github.com>
2023-10-04 20:33:07 +0300

    Update swing_arm.go

--- packet/swing_arm.go
+++ packet/swing_arm.go
@@ -1,17 +1,17 @@
 package packet
 
-type SwingArmServer struct {
+type SwingArm struct {
 	Hand int32
 }
 
-func (s SwingArmServer) ID() int32 {
+func (s SwingArm) ID() int32 {
 	return 0x2F
 }
 
-func (s *SwingArmServer) Decode(r *Reader) error {
+func (s *SwingArm) Decode(r *Reader) error {
   return r.VarInt(&s.Hand)
 }
 
-func (s SwingArmServer) Encode(w Writer) error {
+func (s SwingArm) Encode(w Writer) error {
 	return w.VarInt(s.Hand)
 }
