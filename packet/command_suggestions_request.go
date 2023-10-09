447b302e26f41824fb1009ccec3ff3eb246437f7
oq <84847714+oq-x@users.noreply.github.com>
2023-10-09 18:53:37 +0300

    Command Suggestions Request

---
+++ packet/command_suggestions_request.go
@@ -0,0 +1,20 @@
+package packet
+
+type CommandSuggestionsRequest struct {
+	TransactionId   int32
+  Text string
+}
+
+func (m CommandSuggestionsRequest) ID() int32 {
+	return 0x09
+}
+
+func (m *CommandSuggestionsRequest) Decode(r *Reader) error {
+	r.VarInt(&m.TransactionId)
+	return r.String(&m.Text)
+}
+
+func (m CommandSuggestionsRequest) Encode(w Writer) error {
+	w.VarInt(m.TransactionId)
+	return w.String(m.Text)
+}
