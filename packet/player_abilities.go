3d2b075a9ec2a70dc2b03b9ad5e52d515a0f1eaa
oq <84847714+oq-x@users.noreply.github.com>
2023-10-12 19:46:06 +0300

    Create player_abilities.go

---
+++ packet/player_abilities.go
@@ -0,0 +1,22 @@
+package packet
+
+type PlayerAbilities struct {
+	Flags byte
+  FlyingSpeed float32
+  FieldOfViewModifier float32
+}
+
+func (p PlayerAbilities) ID() int32 {
+	return 0x34
+}
+
+func (p *PlayerAbilities) Decode(r *Reader) error {
+	_ = r.Uint8(&p.Flags)
+  _ = r.Float32(&p.FlyingSpeed)
+	return r.Float32(&p.FieldOfViewModifier)
+}
+func (p PlayerAbilities) Encode(w Writer) error {
+	_ = w.Uint8(p.Flags)
+  _ = w.Float32(p.FlyingSpeed)
+	return w.Float32(p.FieldOfViewModifier)
+}
