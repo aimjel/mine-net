d096fbc23e0df70989db4fa7c65fd9a4bb554671
oq <84847714+oq-x@users.noreply.github.com>
2023-10-13 13:56:19 +0300

    Client Command

---
+++ packet/client_command.go
@@ -0,0 +1,17 @@
+package packet
+
+type ClientCommandServer struct {
+  ActionID int32
+}
+
+func (m ClientCommandServer) ID() int32 {
+	return 0x07
+}
+
+func (m *ClientCommandServer) Decode(r *Reader) error {
+  return r.VarInt(&m.ActionID)
+}
+
+func (m ClientCommandServer) Encode(w Writer) error {
+	return w.VarInt(m.ActionID)
+}
