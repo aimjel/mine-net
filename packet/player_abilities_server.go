02d7c5d000cd15efe61ffd955ffcdbaec8e5abd2
oq <84847714+oq-x@users.noreply.github.com>
2023-10-15 11:01:12 +0300

    Create player_abilities_server.go

---
+++ packet/player_abilities_server.go
@@ -0,0 +1,16 @@
+package packet
+
+type PlayerAbilitiesServer struct {
+	Flags               byte
+}
+
+func (p PlayerAbilitiesServer) ID() int32 {
+	return 0x1C
+}
+
+func (p *PlayerAbilitiesServer) Decode(r *Reader) error {
+	return r.Uint8(&p.Flags)
+}
+func (p PlayerAbilitiesServer) Encode(w Writer) error {
+	return w.Uint8(p.Flags)
+}
