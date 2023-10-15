5c0541617daf61954fdaad21deb441af44829596
oq <84847714+oq-x@users.noreply.github.com>
2023-10-15 11:38:13 +0300

    Block Update

---
+++ packet/block_update.go
@@ -0,0 +1,20 @@
+package packet
+
+type BlockUpdate struct {
+    Location int64
+    BlockID int32
+}
+
+func (c BlockUpdate) ID() int32 {
+	return 0x0A
+}
+
+func (c *BlockUpdate) Decode(r *Reader) error {
+	r.Int64(&c.Location)
+	return r.VarInt(&c.BlockID)
+}
+
+func (c BlockUpdate) Encode(w Writer) error {
+	w.Int64(c.Location)
+	return w.VarInt(c.BlockID)
+}
