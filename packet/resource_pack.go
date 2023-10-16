8fdb0d4bb63b1c4d52f010b93a704df0847bbe95
oq <84847714+oq-x@users.noreply.github.com>
2023-10-16 15:42:52 +0300

    Resource Pack

---
+++ packet/resource_pack.go
@@ -0,0 +1,32 @@
+package packet
+
+import "github.com/aimjel/minecraft/chat"
+
+type ResourcePack struct {
+	URL string
+  Hash string
+  Forced bool
+  Prompt string
+}
+
+func (c ResourcePack) ID() int32 {
+	return 0x40
+}
+
+func (c *ResourcePack) Decode(r *Reader) error {
+  //todo implement
+	return nil
+}
+
+func (c ResourcePack) Encode(w Writer) error {
+  msg := chat.NewMessage(c.Prompt)
+	w.String(c.URL)
+  w.String(c.Hash)
+  w.Bool(c.Forced)
+  if c.Prompt != "" {
+    w.Bool(true)
+    w.String(msg)
+  } else {
+    w.Bool(false)
+  }
+}
