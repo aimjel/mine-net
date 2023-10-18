40c3d0984880bf3c389a8738f8eb7846911c591a
oq <84847714+oq-x@users.noreply.github.com>
2023-10-18 05:37:37 +0300

    Update pool.go

--- pool.go
+++ pool.go
@@ -21,12 +21,13 @@ func (b basicPool) Get(id int32) packet.Packet {
 var serverBoundPlayPool = map[int32]func() packet.Packet{
 	0x04: func() packet.Packet { return &packet.ChatCommandServer{} },
 	0x05: func() packet.Packet { return &packet.ChatMessageServer{} },
+	0x06: func() packet.Packet { return &packet.PlayerSessionServer{} },
 	0x07: func() packet.Packet { return &packet.ClientCommandServer{} },
 	0x08: func() packet.Packet { return &packet.ClientSettings{} },
 	0x09: func() packet.Packet { return &packet.CommandSuggestionsRequest{} },
-
 	0x10: func() packet.Packet { return &packet.InteractServer{} },
 
+	0x12: func() packet.Packet { return &packet.KeepAlive{} },
 	0x14: func() packet.Packet { return &packet.PlayerPosition{} },
 	0x15: func() packet.Packet { return &packet.PlayerPositionRotation{} },
 	0x16: func() packet.Packet { return &packet.PlayerRotation{} },
@@ -38,18 +39,16 @@ var serverBoundPlayPool = map[int32]func() packet.Packet{
 
 	0x24: func() packet.Packet { return &packet.ResourcePackResult{} }, 
 
-	0x12: func() packet.Packet { return &packet.KeepAlive{} },
-
-	0x4d: func() packet.Packet { return &packet.HeldItemChange{} },
-
 	0x2b: func() packet.Packet { return &packet.CreateInventoryAction{} },
+	
+	0x2F: func() packet.Packet { return &packet.SwingArmServer{} },
 
 	0x30: func() packet.Packet { return &packet.TeleportToEntityServer{} },
 
-	0x2F: func() packet.Packet { return &packet.SwingArmServer{} },
-
 	0x31: func() packet.Packet { return &packet.PlayerBlockPlacement{} },
 	0x32: func() packet.Packet { return &packet.UseItem{} },
+
+	0x4d: func() packet.Packet { return &packet.HeldItemChange{} },
 }
 
 type nopPool struct{}
