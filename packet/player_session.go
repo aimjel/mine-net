f38afc454e7583dccb0163cb5a3cf9c26ed26c94
oq <84847714+oq-x@users.noreply.github.com>
2023-10-18 05:35:58 +0300

    Player Session

---
+++ packet/player_session.go
@@ -0,0 +1,30 @@
+package packet
+
+type PlayerSessionServer struct {
+	SessionID [16]byte
+  PublicKey PublicKey
+}
+
+type PublicKey struct {
+  ExpiresAt int64
+  PublicKey []byte
+  KeySignature []byte
+}
+
+func (m PlayerSessionServer) ID() int32 {
+	return 0x06
+}
+
+func (m *PlayerSessionServer) Decode(r *Reader) error {
+	r.UUID(&m.SessionID)
+  r.Int64(&m.ExpiresAt)
+  r.ByteArray(&m.PublicKey)
+  return r.ByteArray(&m.KeySignature)
+}
+
+func (m PlayerSessionServer) Encode(w Writer) error {
+	w.UUID(m.SessionID)
+  w.Int64(m.ExpiresAt)
+  w.ByteArray(m.PublicKey)
+  return r.ByteArray(m.KeySignature)
+}
