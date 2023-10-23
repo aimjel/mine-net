dd6657ceca8376711258fb4a64b3eb4159ef8292
oq <84847714+oq-x@users.noreply.github.com>
2023-10-23 05:11:47 +0300

    Update set_creative_mode_slot.go

--- packet/set_creative_mode_slot.go
+++ packet/set_creative_mode_slot.go
@@ -2,8 +2,13 @@ package packet
 
 type SetCreativeModeSlot struct {
 	Slot int16
-  //ClickedItem Slot
-  //TODO clicked item
+  	ClickedItem Slot
+  //TODO clicked item tag
+}
+
+type Slot struct {
+	Count int8   
+	Id    int32
 }
 
 func (c SetCreativeModeSlot) ID() int32 {
@@ -11,7 +16,14 @@ func (c SetCreativeModeSlot) ID() int32 {
 }
 
 func (c *SetCreativeModeSlot) Decode(r *Reader) error {
-	return r.Int16(&c.Slot)
+	r.Int16(&c.Slot)
+	var present bool
+	r.Bool(&present)
+	if present {
+		r.VarInt(&c.ClickedItem.Id)
+		r.Int8(&c.ClickedItem.Count)
+	}
+	return nil
 }
 
 func (c SetCreativeModeSlot) Encode(w Writer) error {
