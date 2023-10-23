afd9635497a073b2dc8b914dbc4711587ee18cb9
oq <84847714+oq-x@users.noreply.github.com>
2023-10-23 11:25:55 +0300

    Disguised Chat Message

---
+++ packet/disguised_chat_message.go
@@ -0,0 +1,31 @@
+package packet
+
+import "github.com/aimjel/minecraft/chat"
+
+type DisguisedChatMessage struct {
+	Message string
+  ChatType int32
+  ChatTypeName string
+  TargetName string
+}
+
+func (m DisguisedChatMessage) ID() int32 {
+	return 0x1B
+}
+
+func (m *DisguisedChatMessage) Decode(r *Reader) error {
+	return r.String(&m.Content)
+}
+
+func (m DisguisedChatMessage) Encode(w Writer) error {
+	content := chat.NewMessage(m.Message)
+	w.String(content.String())
+	w.VarInt(m.ChatType)
+  w.String(m.ChatTypeName)
+  if m.TargetName != "" {
+    w.Bool(false)
+  } else {
+    w.Bool(true)
+    w.String(m.TargetName)
+  }
+}
