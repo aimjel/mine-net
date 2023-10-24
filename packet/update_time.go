e381eef19f73605bcfe5ea6e3ca9c13091bcf788
oq <84847714+oq-x@users.noreply.github.com>
2023-10-24 17:32:18 +0300

    Update Time

---
+++ packet/update_time.go
@@ -0,0 +1,20 @@
+package packet
+
+type UpdateTime struct {
+	WorldAge int64
+  TimeOfDay int64
+}
+
+func (c UpdateTime) ID() int32 {
+	return 0x5E
+}
+
+func (c *UpdateTime) Decode(r *Reader) error {
+	r.Int64(&c.WorldAge)
+	return r.Int64(&c.TimeOfDay)
+}
+
+func (c UpdateTime) Encode(w Writer) error {
+	w.Int64(c.WorldAge)
+	return w.Int64(c.TimeOfDay)
+}
