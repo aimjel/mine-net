4a56285a4a6d81df743d9d879b9a622c59ec7f3c
oq <84847714+oq-x@users.noreply.github.com>
2023-10-24 22:40:04 +0300

    Change Difficulty

---
+++ packet/change_difficulty.go
@@ -0,0 +1,20 @@
+package packet
+
+type ChangeDifficulty struct {
+	Difficulty uint8
+  DifficultyLocked bool
+}
+
+func (m ChangeDifficulty) ID() int32 {
+	return 0x0C
+}
+
+func (m *ChangeDifficulty) Decode(r *Reader) error {
+	r.Uint8(&m.Difficulty)
+  return r.Bool(&m.DifficultyLocked)
+}
+
+func (m ChangeDifficulty) Encode(w Writer) error {
+	w.Uint8(m.Difficulty)
+  return w.Bool(m.DifficultyLocked)
+}
