1072ea7ce04fb6f12bd905f41138c65b36c2f136
Angel <angelg2181@icloud.com>
2023-10-28 13:01:06 -0400

    paddle boat packet and separated keep alive packets

--- packet/keep_alive.go
+++ packet/keep_alive.go
@@ -1,17 +1,33 @@
 package packet
 
-type KeepAlive struct {
+type KeepAliveServer struct {
 	PayloadID int64
 }
 
-func (a *KeepAlive) ID() int32 {
-	return 0x23
+func (a *KeepAliveServer) ID() int32 {
+	return 0x12
 }
 
-func (a *KeepAlive) Encode(w Writer) error {
+func (a *KeepAliveServer) Encode(w Writer) error {
 	return w.Int64(a.PayloadID)
 }
 
-func (a *KeepAlive) Decode(r *Reader) error {
+func (a *KeepAliveServer) Decode(r *Reader) error {
+	return r.Int64(&a.PayloadID)
+}
+
+type KeepAliveClient struct {
+	PayloadID int64
+}
+
+func (a *KeepAliveClient) ID() int32 {
+	return 0x23
+}
+
+func (a *KeepAliveClient) Encode(w Writer) error {
+	return w.Int64(a.PayloadID)
+}
+
+func (a *KeepAliveClient) Decode(r *Reader) error {
 	return r.Int64(&a.PayloadID)
 }
---
+++ packet/paddle_boat.go
@@ -0,0 +1,20 @@
+package packet
+
+type PaddleBoat struct {
+	LeftPaddleTurning  bool
+	RightPaddleTurning bool
+}
+
+func (p PaddleBoat) ID() int32 {
+	return 0x19
+}
+
+func (p *PaddleBoat) Decode(r *Reader) error {
+	r.Bool(&p.LeftPaddleTurning)
+	return r.Bool(&p.RightPaddleTurning)
+}
+
+func (p *PaddleBoat) Encode(w Writer) error {
+	w.Bool(p.LeftPaddleTurning)
+	return w.Bool(p.RightPaddleTurning)
+}
