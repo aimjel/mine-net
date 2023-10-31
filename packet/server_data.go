9e39f32c137a9bed76aaefc77b520abb775567b6
oq <84847714+oq-x@users.noreply.github.com>
2023-11-01 01:19:32 +0200

    Server Data

---
+++ packet/server_data.go
@@ -0,0 +1,28 @@
+package packet
+
+import "github.com/aimjel/minecraft/chat"
+
+type ServerData struct {
+  MOTD chat.Message
+  Icon []byte
+  EnforcesSecureChat bool
+}
+
+func (m ServerData) ID() int32 {
+	return 0x45
+}
+
+func (m *ServerData) Decode(r *Reader) error {
+  return NotImplemented
+}
+
+func (m ServerData) Encode(w Writer) error {
+   w.String(m.MOTD.String())
+   if m.Icon == nil {
+     w.Bool(false)
+   } else {
+     w.Bool(true)
+     w.ByteArray(m.Icon)
+   }
+   w.Bool(m.EnforcesSecureChat)
+}
