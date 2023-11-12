0430e972135fdd251c28329318ca8d70c257dd85
oq <84847714+oq-x@users.noreply.github.com>
2023-11-12 14:48:45 +0200

    Update player_action.go

--- packet/player_action.go
+++ packet/player_action.go
@@ -2,7 +2,7 @@ package packet
 
 type PlayerActionServer struct {
 	Status   int32
-	Location uint64
+	Location int64
 	Face     int8
 	Sequence int32
 }
@@ -13,14 +13,14 @@ func (m PlayerActionServer) ID() int32 {
 
 func (m *PlayerActionServer) Decode(r *Reader) error {
 	r.VarInt(&m.Status)
-	r.Uint64(&m.Location)
+	r.Int64(&m.Location)
 	r.Int8(&m.Face)
 	return r.VarInt(&m.Sequence)
 }
 
 func (m PlayerActionServer) Encode(w Writer) error {
 	w.VarInt(m.Status)
-	w.Uint64(m.Location)
+	w.Int64(m.Location)
 	w.Int8(m.Face)
 	return w.VarInt(m.Sequence)
 }
