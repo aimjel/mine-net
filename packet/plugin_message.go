f0751c927ecb9531d74b90ac6538e9f7ccfc98dd
oq <84847714+oq-x@users.noreply.github.com>
2024-12-31 20:54:45 +0200

    Update plugin_message.go

--- packet/plugin_message.go
+++ packet/plugin_message.go
@@ -2,21 +2,40 @@ package packet
 
 import "github.com/aimjel/minecraft/protocol/encoding"
 
-type PluginMessage struct {
+type PlayClientboundPluginMessage struct {
 	Channel string
 	Data    []byte
 }
 
-func (p PluginMessage) ID() int32 {
-	return 0x17
+func (p PlayClientboundPluginMessage) ID() int32 {
+	return 0x19
 }
 
-func (p PluginMessage) Decode(r *encoding.Reader) error {
+func (p PlayClientboundPluginMessage) Decode(r *encoding.Reader) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (p PluginMessage) Encode(w *encoding.Writer) error {
+func (p PlayClientboundPluginMessage) Encode(w *encoding.Writer) error {
 	_ = w.String(p.Channel)
-	return w.ByteArray(p.Data)
+	return w.FixedByteArray(p.Data)
+}
+
+type ConfigurationClientboundPluginMessage struct {
+	Channel string
+	Data    []byte
+}
+
+func (p ConfigurationClientboundPluginMessage) ID() int32 {
+	return 0x01
+}
+
+func (p ConfigurationClientboundPluginMessage) Decode(r *encoding.Reader) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (p ConfigurationClientboundPluginMessage) Encode(w *encoding.Writer) error {
+	_ = w.String(p.Channel)
+	return w.FixedByteArray(p.Data)
 }
